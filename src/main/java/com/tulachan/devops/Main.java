f2264bef7f6de04c2cbb14964e91cbddc55555af
Ameet <Ameet@192.168.0.106>
2016-08-21 12:30:05 -0700

    adding a first java file

---
+++ src/main/java/com/tulachan/devops/Main.java
@@ -0,0 +1,11 @@
+package main.java.com.tulachan.devops;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What up nigga");
+
+	}
+
+}
